461f6eaba2991088ae5d53280972c9a1369be53c
Cesar <cesar777welch@gmail.com>
2016-04-04 05:55:48 -0600

    Done
    Finally

--- Planner/Form1.Designer.cs
+++ Planner/Form1.Designer.cs
@@ -29,33 +29,139 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 68);
+            this.button1.Location = new System.Drawing.Point(12, 89);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(91, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Generar Calendario";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(213, 427);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(351, 68);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(239, 427);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hora, clase, maestro, aula";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Horario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(351, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Prediccion por clase";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(351, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Clase, ID de alumnos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(602, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Satisfaccion Promedio";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(616, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "-";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(858, 531);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Generador";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Planner/Form1.cs
+++ Planner/Form1.cs
@@ -17,7 +17,7 @@ namespace Planner
         public Form1()
         {
             InitializeComponent();
-            do_some();
+
         }
 
         //logica de todo
@@ -25,17 +25,17 @@ namespace Planner
         private void do_some()
         {
             bool[,] aula_tiempo = new bool[9, 280];
-            
+
             for (int i = 0; i < 9; i++)//Rows
             {
                 for (int j = 0; j < 280; j++)//Columns
                 {
-                    aula_tiempo[i,j] = false;
+                    aula_tiempo[i, j] = false;
                     //Console.Write(" "+i+","+j+" ");
                 }
-              //  Console.WriteLine("\n");
+                //  Console.WriteLine("\n");
             }
-            
+
 
             //Clases Disponibles: 
             List<course> courses = new List<course>();
@@ -68,7 +68,7 @@ namespace Planner
                 {
 
                     //file reading
-                    Console.WriteLine(Environment.CurrentDirectory+"  hi");
+                    Console.WriteLine(Environment.CurrentDirectory + "  hi");
                     string[] lines = System.IO.File.ReadAllLines(Environment.CurrentDirectory + @"\maestro por codigo de clase.csv", Encoding.UTF8);
                     foreach (string line in lines)
                     {
@@ -153,7 +153,7 @@ namespace Planner
                                         courses.Last().hour = s;
                                         for (int au = 0; au < 280; au++)
                                         {
-                                            if (aula_tiempo[s, au]==false)
+                                            if (aula_tiempo[s, au] == false)
                                             {
                                                 aula_tiempo[s, au] = true;
                                                 courses.Last().nclass = au;
@@ -180,53 +180,69 @@ namespace Planner
             }
             int sa = 0;
             // maestro y clases
+            Console.WriteLine("El horario de clases junto con su maestro y aula");
+            textBox1.AppendText("El horario de clases junto con su maestro y aula \n");
             for (int i = 0; i < courses.Count; i++)
             {
                 sa += courses[i].students.Count;
-               Console.WriteLine(courses[i] );
+                Console.WriteLine(courses[i]);
+                textBox1.AppendText(courses[i].ToString() + "\n");
             }
             //prediccion
-            
-                         for (int i = 0; i < courses.Count; i++)
-                         {
-                             Console.Write(courses[i].id );
-                             sa += courses[i].students.Count;
-                             for (int j = 0; j < courses[i].students.Count; j++)
-                             {
-                                 Console.Write("," + courses[i].students[j]);
-                             }
-                Console.WriteLine();
+            Console.WriteLine("Prediccion por Curso");
+        
+            for (int i = 0; i < courses.Count; i++)
+            {
+                Console.Write(courses[i].id);
+                textBox2.AppendText(courses[i].id);
 
-                         }
+                sa += courses[i].students.Count;
+                for (int j = 0; j < courses[i].students.Count; j++)
+                {
+                    Console.Write("," + courses[i].students[j]);
+                    textBox2.AppendText("," + courses[i].students[j] );
+                }
+                Console.WriteLine();
+                textBox2.AppendText("\n");
+
+            }
 
 
             //satisfaccion
-            int satis=0;
-            decimal avg=0;
+            Console.WriteLine("Satisfaccion Promedio de alumno matriculado");
+            decimal avg = 0;
             decimal stu = 0;
             for (int i = 0; i < students.Count; i++)
             {
                 int calc = 0;
                 calc = (students[i].getbusymax() - students[i].getbusymin());
-                if (calc<0)
+                if (calc < 0)
                 {
-                    satis = 0;
-                   // avg += 0;
-               //     Console.WriteLine(students[i].id + ",N/A");
 
-                }else {
-                    decimal calc2 = ((decimal)students[i].busy.Count() / (decimal)students[i].arrcourses.Count())*3m;
-                    decimal calc3 = ((decimal)students[i].getbusymax() - (decimal)students[i].getbusymin())/8m*2m;
-                    decimal calc4 = ((decimal)calc2 + (decimal)calc3);
-                      Console.WriteLine(students[i].id + "," +calc4);
+                    // avg += 0;
+                    //     Console.WriteLine(students[i].id + ",N/A");
+
+                }
+                else {
+                    decimal calc2 = ((decimal)students[i].busy.Count() / (decimal)students[i].courses.Count()) * 3m;
+                    int calc3 = (students[i].getbusymax() - students[i].getbusymin());
+                    decimal count = 2;
+                    for (int k = 0; k < calc3; k++)
+                    {
+                        count -= 0.22m;
+                    }
+                    // Console.WriteLine(students[i].id + "," + ((decimal)students[i].busy.Count() / (decimal)students[i].arrcourses.Count()) * 3m + " " + ((decimal)students[i].getbusymax() - (decimal)students[i].getbusymin()) * 0.25m);
+                    decimal calc4 = count + calc2;
                     stu++;
                     avg += calc4;
                 }
-                
-                
+
+
                 calc = 0;
             }
-            Console.WriteLine(avg);
+            decimal labelsix = avg / stu;
+            label6.Text = (labelsix.ToString());
+            Console.WriteLine(avg / stu);
         }
         //            List<student> students = new List<student>();
         //            List<teacher> teachers = new List<teacher>();
@@ -236,8 +252,17 @@ namespace Planner
 
         private void button1_Click(object sender, EventArgs e)
         {
+            do_some();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
 
         }
 
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
